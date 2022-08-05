67e141d2f3f8939787c5a25de430e9fe450dbde5
FlameDavid92 <davidefiguccia2020@outlook.it>
2022-08-05 09:37:44 +0200

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from "@angular/core";
-import { NgForm } from "@angular/forms";
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 interface RegistrationData {
   email: string;
@@ -8,32 +8,30 @@ interface RegistrationData {
 }
 
 @Component({
-  selector: "my-app",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"],
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   submitted = false;
   registrationData: RegistrationData = {
-    email: "",
-    subscription: "",
-    password: "",
+    email: '',
+    subscription: '',
+    password: '',
   };
 
   onSubmit(form: NgForm) {
     !this.submitted && (this.submitted = true);
     console.log(form.value);
-    console.log(form);
-
     this.registrationData = { ...form.value };
-    // form.reset();
+    form.reset();
   }
 
   onClearData() {
     this.registrationData = {
-      email: "",
-      subscription: "",
-      password: "",
+      email: '',
+      subscription: '',
+      password: '',
     };
     this.submitted = false;
   }
